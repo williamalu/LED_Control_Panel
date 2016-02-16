3f30a69696bf0d32dcb5a35682ed2ad97d794643
shanek21 <shanegkelly21@gmail.com>
2016-02-16 03:39:42 +0000

    Added 5 new buttons

--- runTerminalCommand.js
+++ runTerminalCommand.js
@@ -23,4 +23,29 @@ $(document).ready(function(){
                 $.post('sendTerminalCommand.php',{data: "4"},function(response){
                 });
 	});
+
+	$('#rainbow').click(function(){
+                $.post('sendTerminalCommand.php',{data: "5"},function(response){
+                });
+	});
+
+	$('#redChase').click(function(){
+                $.post('sendTerminalCommand.php',{data: "6"},function(response){
+                });
+	});
+
+	$('#greenChase').click(function(){
+                $.post('sendTerminalCommand.php',{data: "7"},function(response){
+                });
+	});
+
+	$('#blueChase').click(function(){
+                $.post('sendTerminalCommand.php',{data: "8"},function(response){
+                });
+	});
+
+	$('#rainbowChase').click(function(){
+                $.post('sendTerminalCommand.php',{data: "9"},function(response){
+                });
+	});
 });
