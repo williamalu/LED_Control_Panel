62ac3abb423bae1d0d431c2d6925c06b2857cf7e
shanek21 <shanegkelly21@gmail.com>
2016-09-29 23:42:52 -0400

    Change light state numbers in jQuery code

--- runTerminalCommand.js
+++ runTerminalCommand.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
 		
 		$('#off').click(function(){
-        $.post('sendTerminalCommand.php',{data: "{0}"});
+        $.post('sendTerminalCommand.php',{data: "0"});
 		});
 
 		$('#on').click(function(){
-        $.post('sendTerminalCommand.php',{data: "b"});
+        $.post('sendTerminalCommand.php',{data: "1"});
 		});
 
 		$('#red').click(function(){
-        $.post('sendTerminalCommand.php',{data: "c"});
+        $.post('sendTerminalCommand.php',{data: "2"});
 		});
 
 		$('#green').click(function(){
-        $.post('sendTerminalCommand.php',{data: "d"});
+        $.post('sendTerminalCommand.php',{data: "3"});
 		});
 
 		$('#blue').click(function(){
-        $.post('sendTerminalCommand.php',{data: "e"});
+        $.post('sendTerminalCommand.php',{data: "4"});
 		});
 
 		$('#fullRainbow').click(function(){
-        $.post('sendTerminalCommand.php',{data: "f"});
+        $.post('sendTerminalCommand.php',{data: "5"});
 		});
 
     $('#rainbow').click(function(){
-        $.post('sendTerminalCommand.php',{data: "g"});
+        $.post('sendTerminalCommand.php',{data: "6"});
     });
 
 		$('#redChase').click(function(){
-        $.post('sendTerminalCommand.php',{data: "h"});
+        $.post('sendTerminalCommand.php',{data: "7"});
 		});
 
 		$('#greenChase').click(function(){
-        $.post('sendTerminalCommand.php',{data: "i"});
+        $.post('sendTerminalCommand.php',{data: "8"});
 		});
 
 		$('#blueChase').click(function(){
-        $.post('sendTerminalCommand.php',{data: "j"});
+        $.post('sendTerminalCommand.php',{data: "9"});
 		});
 
 		$('#random').click(function(){
-        $.post('sendTerminalCommand.php',{data: "k"});
+        $.post('sendTerminalCommand.php',{data: "10"});
 		});
 
 		$(document).keypress(function(e) {
